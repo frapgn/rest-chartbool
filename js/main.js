b480dd969758569bd691922b9a23ec687afbbcf4
frapgn <59645305+frapgn@users.noreply.github.com>
2020-04-07 22:50:52 +0200

    il commit precedente era sbagliato

--- js/main.js
+++ js/main.js
@@ -1,17 +1,31 @@
 
 var salesArchive = {};
 
+var salesByMonth = {
+    gennaio: 0,
+    febbraio: 0,
+    marzo: 0,
+    aprile: 0,
+    maggio: 0,
+    giugno: 0,
+    luglio: 0,
+    agosto: 0,
+    settembre: 0,
+    ottobre: 0,
+    novembre: 0,
+    dicembre: 0,
+}
+
 $.ajax({
     url: 'http://157.230.17.132:4024/sales',
     method: 'GET',
     success: function(data) {
-        for (var i = 0; i < 2; i++) {
-            var date = moment().month(data[i].date).locale('it').format('MMMM');
-            console.log(date);
-            if(salesArchive[date] === undefined){
-                salesArchive[date] = date;
-            }
-            // console.log(salesArchive);
+        for (var i = 0; i < 3; i++) {
+            var originalDate = moment(data[i].date, 'DD-MM-YYYY').locale('it');
+            console.log(originalDate);
+            var month = originalDate.format('MMMM');
+            console.log(month);
+
         }
     }
 });
